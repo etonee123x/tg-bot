a6e71f2c1d66c4fa8855ac435b0e9a4b71ac9d55
Evgeniy Uvarov <69967430+etonee123x@users.noreply.github.com>
2024-06-17 23:01:37 +0400

    Update index.ts

--- src/commands/Auth/index.ts
+++ src/commands/Auth/index.ts
@@ -14,13 +14,16 @@ const params: CommandParams = {
     default: '',
   },
 };
+
 export default class Auth extends GenericCommand {
   private readonly jwt: string;
   private readonly pattern: string;
 
   constructor(commandBody?: string) {
     super(params, commandBody);
+
     const isLong = this.getValueForParam('long');
+
     this.pattern = this.getValueForParam('pattern')
     this.jwt = jwt.sign({ role: 'Admin' }, String(process.env.SECRET_KEY), { expiresIn: isLong ? '1d' : '1h' });
   }
